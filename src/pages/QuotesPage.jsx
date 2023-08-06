be7d6f8e63d277e7dae8c71010bd93466e6acb4a
yohanesrioirsan <rioirsan8@gmail.com>
2023-08-07 02:38:51 +0700

    feat(anime quotes generator)

---
+++ src/pages/QuotesPage.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../styles/global.css";
+
+function QuotesPage() {
+  const [quotesData, setQuotesData] = useState();
+
+  const fetchQuotesData = async () => {
+    try {
+      const response = await axios.get("https://animechan.xyz/api/random");
+      setQuotesData(response.data);
+    } catch (error) {
+      console.log("Error : ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuotesData();
+  }, []);
+
+  const handleGenerate = () => {
+    fetchQuotesData();
+  };
+
+  return (
+    <section className="quotes-page">
+      <div className="quotes-wrapper">
+        <div className="content">
+          <h1>
+            {quotesData?.quote} - <span>{quotesData?.character}</span> from{" "}
+            <span>{quotesData?.anime}</span>
+          </h1>
+          <button className="btn-generate" onClick={handleGenerate}>
+            Generate Other
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default QuotesPage;
